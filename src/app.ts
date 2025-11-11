eb0b00281bab6ef064e7257dfbc99b4bfcfe3e50
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2025-11-11 11:26:12 +0200

    feat: adding rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import { Logger, LogLevel } from './utils/winston_log';
 import { errorHandler } from './middlewares/errorHandler';
 import { sendBaseError } from './utils/response';
@@ -14,6 +15,23 @@ app.use(bodyParser.urlencoded());
 
 app.use(cors());
 app.use(express.json());
+
+// Set up rate limiter: maximum of 5 requests per 1 minute per IP
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 5 requests per `window` (here, per 1 minute)
+  handler: (req, res) => {
+    return sendBaseError(
+      res,
+      ['Rate limit exceeded'],
+      'You have exceeded the number of allowed requests. Please try again later.',
+      429
+    );
+  },
+});
+
+// Apply the rate limiter to all requests
+app.use(limiter);
 
 app.use(
   morgan(`:method :url :status :res[content-length] - :response-time ms`, {
